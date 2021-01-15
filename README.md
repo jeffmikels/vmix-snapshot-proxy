746d68afcb77d43103897d59e004de6745c562b2
Jeff Mikels <jeffweb@mikels.cc>
2021-01-15 00:03:04 -0500

    installation instructions

---
+++ README.md
@@ -0,0 +1,56 @@
+# vMix Snapshot Proxy
+
+
+vMix has a robust API, but one key limitation of the API is that while you can
+tell vMix to take a snapshot of an input, it will save the image on the vMix
+machine but not send the image over the network.
+
+This is a small application running on the nodejs framework to act as a proxy
+for those images.
+
+The application is especially helpful in providing preview images of each input for
+
+[Unofficial vMix Remote Control](https://play.google.com/store/apps/details?id=org.jeffmikels.vmix_remote)
+
+
+
+## Installation
+
+These commands should be run on the same computer that is running vMix.
+
+```bash
+$ git clone https://github.com/jeffmikels/vmix-snapshot-proxy.git
+$ cd vmix-snapshot-proxy
+$ npm install
+```
+
+## Usage
+
+Edit the settings at the top of `index.js`.
+
+Start vMix and then the proxy server...
+
+```
+$ node index.js
+```
+
+You'll see something like the following output
+
+```
+=====================================
+Running vMix Snapshot Proxy at port 8098
+Get a list of all inputs:                http://192.168.1.1:8098/
+Force regen one input (0 means program): http://192.168.1.1:8098/regen/#
+Force regen all inputs:                  http://192.168.1.1:8098/regen
+Get input snapshot:                      http://192.168.1.1:8098/#.jpg
+
+Getting an input snapshot sends the most recent snapshot, and queues the generation of a new one.
+Snapshots take about 1 second to process
+=====================================
+```
+
+Open a browser and visit:
+
+`http://[PROXY_IP_ADDRESS]:[PROXY_PORT]/[INPUT_NUMBER].jpg`
+
+Every time you visit that address, you will receive a new snapshot image of the selected input.
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+
+// EDIT THESE SETTINGS IF YOU NEED TO =================
+
+const vMixStorage = 'C:/Users/AV/Documents/vMixStorage/'  // should agree with settings in vMix
+const vmixUrl = 'http://localhost:8088/api';              // only change this if proxy is on a different computer
+const proxyRunsOnPort = 8098                              // which port does the proxy listen on
+
+// DO NOT EDIT BELOW THIS LINE ========================
+
+
+// REQUIRED MODULES
+const ifaces = require('os').networkInterfaces()
 const parser = require( 'fast-xml-parser' );
 const got = require( 'got' );
 const express = require( 'express' );
 const nocache = require( 'nocache' );
 const print = console.log;
 
-const vMixStorage = 'C:/Users/AV/Documents/vMixStorage/'
-
-const vmixUrl = 'http://localhost:8088/api';
+// GLOBALS
+let myip = 'THIS_COMPUTER_IP';
+for (let key of Object.keys(ifaces)) {
+	let device = ifaces[key][0]
+	if (device.family == 'IPv4' && !device.internal) {
+		myip = device.address;
+		break;
+	}
+}
 
 let inputs = [];
+
+
+// FUNCTIONS
 function sleep( ms ) {
 	return new Promise( resolve => setTimeout( resolve, ms ) );
 }
@@ -16,12 +37,13 @@ function sleep( ms ) {
 function status() {
 	print( '=====================================' )
 	print( 'Running vMix Snapshot Proxy at port 8098' )
-	print( 'Get a list of all inputs: http://barnabas:8098/' )
-	print( 'Force regen one input (0 means program): http://barnabas:8098/regen/#' )
-	print( 'Force regen all inputs: http://barnabas:8098/regen' )
-	print( 'Get input snapshot: http://barnabas:8098/#.jpg' )
-	print( 'Getting an input snapshot sends the most recent snapshot, and queues the generation of a new one.' )
-	print( 'Snapshots take about 1 second to process' )
+	print( `Get a list of all inputs:                http://${myip}:${proxyRunsOnPort}/` )
+	print( `Force regen one input (0 means program): http://${myip}:${proxyRunsOnPort}/regen/#` )
+	print( `Force regen all inputs:                  http://${myip}:${proxyRunsOnPort}/regen` )
+	print( `Get input snapshot:                      http://${myip}:${proxyRunsOnPort}/#.jpg` )
+	print( `` )
+	print( `Getting an input snapshot sends the most recent snapshot, and queues the generation of a new one.` )
+	print( `Snapshots take about 1 second to process` )
 	print( '=====================================' )
 }
 
@@ -54,60 +76,64 @@ async function request_snapshots( inputNumber = -1 ) {
 }
 
 
-// connect to vmix
-get_inputs();
+function main() {
+	// connect to vmix
+	get_inputs();
 
-// express setup
-const app = express();
-app.set( 'etag', false )
-app.use( nocache() );
+	// express setup
+	const app = express();
+	app.set( 'etag', false )
+	app.use( nocache() );
 
-app.get( '/', async ( req, res ) => {
-	await get_inputs();
-	res.json( inputs )
-} );
+	app.get( '/', async ( req, res ) => {
+		await get_inputs();
+		res.json( inputs )
+	} );
 
-app.get( '/regen', async ( req, res ) => {
-	await request_snapshots();
-	res.json( 'snapshots are regenerating' )
-} );
+	app.get( '/regen', async ( req, res ) => {
+		await request_snapshots();
+		res.json( 'snapshots are regenerating' )
+	} );
 
-app.get( '/regen/:input', async ( req, res ) => {
-	await request_snapshots( req.params.input );
-	res.json( 'snapshot ' + req.params.input + ' is regenerating' )
-} );
+	app.get( '/regen/:input', async ( req, res ) => {
+		await request_snapshots( req.params.input );
+		res.json( 'snapshot ' + req.params.input + ' is regenerating' )
+	} );
 
-// express handles middleware in the order it is added
-// handle input image requests
-app.use( async ( req, res, next ) => {
-	// always generate a new image every time
-	print( req.path );
-	let match = req.path.match( /(\d+)\.jpg/ );
-	if ( match ) {
-		let input = match[ 1 ];
-		request_snapshots( input );
-	}
-	// send it on to the static middleware
-	next();
-} );
-app.use( express.static( vMixStorage ) );
-app.use( async ( req, res, next ) => {
-	// file was not found, so wait a little bit
-	await sleep( 1000 );
+	// express handles middleware in the order it is added
+	// handle input image requests
+	app.use( async ( req, res, next ) => {
+		// always generate a new image every time
+		print( req.path );
+		let match = req.path.match( /(\d+)\.jpg/ );
+		if ( match ) {
+			let input = match[ 1 ];
+			request_snapshots( input );
+		}
+		// send it on to the static middleware
+		next();
+	} );
+	app.use( express.static( vMixStorage ) );
+	app.use( async ( req, res, next ) => {
+		// file was not found, so wait a little bit
+		await sleep( 1000 );
 
-	// and try again
-	next();
-} );
-app.use( express.static( vMixStorage ) );
+		// and try again
+		next();
+	} );
+	app.use( express.static( vMixStorage ) );
 
-// app.use( '/:input', async ( req, res, next ) => {
-// 	await request_snapshots( req.params.input );
-// 	print( req );
-// 	req.path = req.params.input + '.jpg';
-// 	next( req, res );
-// }, express.static( vMixStorage ) );
+	// app.use( '/:input', async ( req, res, next ) => {
+	// 	await request_snapshots( req.params.input );
+	// 	print( req );
+	// 	req.path = req.params.input + '.jpg';
+	// 	next( req, res );
+	// }, express.static( vMixStorage ) );
 
-status();
-setInterval( status, 1000 * 60 * 5 );
+	status();
+	setInterval( status, 1000 * 60 * 5 );
+	app.listen( proxyRunsOnPort )
+}
 
-app.listen( 8098 )
+main();
+
